288b9e45b3e75715e7768ad754c3b8c957299138
ss7326 <htky3131@gmail.com>
2024-11-12 00:29:05 +0900

    success to connect to db by mongoose

---
+++ practice/00_connect_mongodb.js
@@ -0,0 +1,47 @@
+// https://github.com/mongodb-developer/nodejs-quickstart/blob/master/connection.js
+
+var mongoose = require("mongoose");
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb://localhost:27017/";
+
+  /**
+   * The Mongo Client you will use to interact with your database
+   * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details
+   * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+   * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+   * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+   */
+
+  try {
+    // Mongooseで接続
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Mongoose接続成功");
+    await listDatabases();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await mongoose.connection.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases() {
+  // mongooseを使用してデータベース一覧を取得
+  const connection = mongoose.connection;
+  const admin = connection.db.admin();
+  const databasesList = await admin.listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
