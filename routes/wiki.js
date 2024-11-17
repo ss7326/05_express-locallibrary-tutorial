fe55c9da6f57b7efe97b25280bcc123513e30100
ss7326 <htky3131@gmail.com>
2024-11-17 11:35:07 +0900

    Create the route-handler callback functions

--- routes/wiki.js
+++ routes/wiki.js
@@ -9,14 +9,13 @@ router.get("/", function (req, res) {
 });
 
 // About page route.
-router.get("/about", (req, res, next) => {
-  About.find({}).exec((err, queryResults) => {
-    if (err) {
-      return next(err);
-    }
-    //Successful, so render
-    res.render("about_view", { title: "About", list: queryResults });
-  });
+router.get("/about", async function (req, res, next) {
+  try {
+    const successfulResult = await About.find({}).exec();
+    res.render("about_view", { title: "About", list: successfulResult });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;
