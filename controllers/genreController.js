c0c4ee7be53cc632d8c1e2428a28c5314f81e5af
a7uku <htky3131@gmail.com>
2024-12-16 01:06:50 +0900

    error, 6-challenge, delete book.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -83,7 +83,22 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  // Get details of genre and all their books (in parallel)
+  const [genre, allBooksByGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre === null) {
+    // No results.
+    res.redirect("/catalog/genres");
+  }
+
+  res.render("genre_delete", {
+    title: "Delete Genre",
+    genre: genre,
+    genre_books: allBooksByGenre,
+  });
 });
 
 // Handle Genre delete on POST.
